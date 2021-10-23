bd75a75b4fc4d5a5f7b04aa0ea297d5d203f4f78
Daniel Bluhm <dbluhm@pm.me>
2021-10-23 17:45:26 -0400

    feat: refine output of wait
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- wait.sh
+++ wait.sh
@@ -5,8 +5,7 @@ TUNNEL_ENDPOINT=${TUNNEL_ENDPOINT:-http://localhost:4040}
 WAIT_INTERVAL=${WAIT_INTERVAL:-3}
 WAIT_ATTEMPTS=${WAIT_ATTEMPTS:-10}
 
-echo "tunnel endpoint [$TUNNEL_ENDPOINT]" 1>&2
-
+echo "fetching endpoint from ${TUNNEL_ENDPOINT}" 1>&2
 for _ in $(seq 1 "$WAIT_ATTEMPTS"); do
     if ! wget --server-response --spider --quiet "${TUNNEL_ENDPOINT}" 2>&1 | grep "200 OK" > /dev/null; then
         echo "Waiting for tunnel..." 1>&2
