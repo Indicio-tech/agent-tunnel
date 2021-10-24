00d0c3b5399fcabd079b5e36177aabd6c5ee1fff
Daniel Bluhm <dbluhm@pm.me>
2021-10-24 17:25:51 -0400

    feat: make service an option
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- index.js
+++ index.js
@@ -3,24 +3,39 @@ const localtunnel = require('localtunnel')
 const morgan = require('morgan')
 const { exit } = require('yargs')
 const yargs = require('yargs')
+const DEFAULT_HOST = 'https://localtunnel.me'
 const argv = yargs
+  .scriptName('agent-tunnel')
   .usage('Usage: $0 <service> [options]')
   .example('$0 service:8080 -p 4040')
-  .demandCommand(1, 'Must enter service')
-  .alias('p', 'port')
-  .describe('p', 'Port to manage tunnel')
-  .describe('host', 'URL for the upstream proxy server')
-  .alias('h', 'help')
-  .help('h')
+  .options({
+    service: {
+      alias: 's',
+      type: 'string',
+      describe: 'service to tunnel to',
+      demandOption: 'Must define service'
+    },
+    port: {
+      alias: 'p',
+      default: 4040,
+      type: 'number',
+      describe: 'Port to manage tunnel'
+    },
+    host: {
+      alias: 'h',
+      default: DEFAULT_HOST,
+      type: 'string',
+      describe: 'URL for the upstream proxy server'
+    }
+  })
+  .help('help')
   .argv
-const [localhost, localport] = argv._[0].split(':')
-const port = argv.port || 4040
-const host = argv.host || 'https://localtunnel.me'
+const [localhost, localport] = argv.service.split(':')
 
 const app = express()
 
 async function main () {
-  const tunnel = await localtunnel({ port: localport, local_host: localhost, host: host })
+  const tunnel = await localtunnel({ port: localport, local_host: localhost, host: argv.host })
   tunnel.on('request', (info) => { console.log(info) })
   tunnel.on('error', (err) => { console.error(err) })
   tunnel.on('close', () => { console.log('Closing tunnel') })
@@ -39,17 +54,17 @@ async function main () {
     res.json({
       host: localhost,
       port: localport,
-      mangement: port,
+      mangement: argv.port,
       status: tunnel === null ? 'stopped' : 'running',
       url: tunnel === null ? null : tunnel.url
     })
   })
 
-  app.listen(port, () => {
-    if (argv.host) {
-      console.log(`Using tunnel server: ${host}`)
+  app.listen(argv.port, () => {
+    if (argv.host !== DEFAULT_HOST) {
+      console.log(`Using tunnel server: ${argv.host}`)
     }
-    console.log(`Tunnel to ${localhost}:${localport} running; mangement available on ${port}`)
+    console.log(`Tunnel to ${localhost}:${localport} running; mangement available on ${argv.port}`)
     console.log(`Tunnel URL is: ${tunnel.url}`)
   })
 }
