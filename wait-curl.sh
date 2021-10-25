21bc2dc1208d85abda4ca616c913a3d1fb300ad7
Daniel Bluhm <dbluhm@pm.me>
2021-10-25 10:15:20 -0400

    feat: add curl based wait script
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ wait-curl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+TUNNEL_ENDPOINT=${TUNNEL_ENDPOINT:-http://localhost:4040}
+WAIT_INTERVAL=${WAIT_INTERVAL:-3}
+WAIT_ATTEMPTS=${WAIT_ATTEMPTS:-10}
+
+echo "fetching endpoint from ${TUNNEL_ENDPOINT}" 1>&2
+for _ in $(seq 1 "$WAIT_ATTEMPTS"); do
+    if ! curl -s -o /dev/null -w '%{http_code}' "${TUNNEL_ENDPOINT}/url" | grep "200"; then
+        echo "Waiting for tunnel..." 1>&2
+        sleep "$WAIT_INTERVAL" &
+        wait $!
+    else
+        break
+    fi
+done
+
+RETRIEVED=$(curl --silent "${TUNNEL_ENDPOINT}/url" | sed -nr 's/\{.*"url":\s*"([^"]*)".*}/\1/p')
+echo "fetched end point [$RETRIEVED]" 1>&2
+echo "$RETRIEVED"
+
